5c93ce1fff450020a5b8d6199e5e578da50d4e9a
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-06-08 12:17:23 +0000

    import states from package into tail_light_fsm module

--- tail_light_fsm/tail_light_fsm.srcs/sources_1/new/fsm_tail_light.sv
+++ tail_light_fsm/tail_light_fsm.srcs/sources_1/new/fsm_tail_light.sv
@@ -19,24 +19,11 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-package states;
-    typedef enum logic [2:0] {
-        S0 = 3'b000, 
-        S1 = 3'b001,
-        S2 = 3'b010, 
-        S3 = 3'b011, 
-        S4 = 3'b100,
-        S5 = 3'b101, 
-        S6 = 3'b110, 
-        S7 = 3'b111
-    } State;
-endpackage
-
-import states::*;
-
 module fsm_tail_light(input logic L, R, clk, rst,
                       output logic RA, RB, RC, LA, LB, LC);
-     
+    
+    import states::*;
+         
     logic clk_en;        
     State [2:0]state_cur, state_nxt;
     
