7e65c01051c6da5159c2933c0b788295c4dbfc00
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-06-08 16:53:08 +0000

    implement addition opcodes

--- alu/alu.srcs/sources_1/new/alu.sv
+++ alu/alu.srcs/sources_1/new/alu.sv
@@ -21,8 +21,8 @@
 import alu_pkg::*;
 
 module alu(
-    input logic [6:0]    a,
-    input logic [6:0]    b,
+    input logic [5:0]    a,
+    input logic [5:0]    b,
     input AluOp          alu_op,
     output logic [6:0]   result,
     output logic         zero
@@ -34,11 +34,18 @@ module alu(
         case(alu_op)
             ADD: temp = a + b; 
             SUB: temp = a - b; 
-            SLL: temp = a << b;     
+            SLL: temp = a << b;
+            SLT: temp = (a < b) ? 1 : 0;
+            SLTU: temp = (a < b) ? 1 : 0;
+            XOR: temp = a ^ b;
+            SRL: temp = a >> b;
+            SRA: temp = a >>> b;
+            OR: temp = a | b;
+            AND: temp = a & b;
         endcase
     end
     
     seven_segment sv(temp, result);
-    assign zero = temp == 0 ? 1 : 0;
+    assign zero = result == 0 ? 1 : 0;
     
 endmodule
