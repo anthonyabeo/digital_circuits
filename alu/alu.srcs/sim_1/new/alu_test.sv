98f616920731f7f2912ffd691968950297546b1f
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-06-09 18:29:57 +0000

    add a few more simulations

--- alu/alu.srcs/sim_1/new/alu_test.sv
+++ alu/alu.srcs/sim_1/new/alu_test.sv
@@ -61,16 +61,33 @@ module alu_test();
         //////////
         a=2; b=1; op=SLL; #10;
         assert(result==4) else $display("ERROR: Test Case 5: op=%b a=%h b=%h. Got result=%h, expected result=4.", op, a, b, result);
-        assert(zero==0) else $display("ERROR: Test Case 4: op=%b a=%h b=%h. Got zero=%h, expected zero=0", op, a, b, zero);
+        assert(zero==0) else $display("ERROR: Test Case 5: op=%b a=%h b=%h. Got zero=%h, expected zero=0", op, a, b, zero);
         
         a=4; b=1; op=SLL; #10;
-        assert(result==8) else $display("ERROR: Test Case 5: op=%b a=%h b=%h. Got result=%h, expected result=8.", op, a, b, result);
-        assert(zero==0) else $display("ERROR: Test Case 4: op=%b a=%h b=%h. Got zero=%h, expected zero=0", op, a, b, zero);
+        assert(result==8) else $display("ERROR: Test Case 6: op=%b a=%h b=%h. Got result=%h, expected result=8.", op, a, b, result);
+        assert(zero==0) else $display("ERROR: Test Case 6: op=%b a=%h b=%h. Got zero=%h, expected zero=0", op, a, b, zero);
         
         a=0; b=0; op=SLL; #10;
-        assert(result==0) else $display("ERROR: Test Case 5: op=%b a=%h b=%h. Got result=%h, expected result=0.", op, a, b, result);
-        assert(zero==1) else $display("ERROR: Test Case 4: op=%b a=%h b=%h. Got zero=%h, expected zero=1", op, a, b, zero);
+        assert(result==0) else $display("ERROR: Test Case 7: op=%b a=%h b=%h. Got result=%h, expected result=0.", op, a, b, result);
+        assert(zero==1) else $display("ERROR: Test Case 7: op=%b a=%h b=%h. Got zero=%h, expected zero=1", op, a, b, zero);
         
+        //////////
+        // SLT
+        //////////
+        a=2; b=4; op=SLT; #10;
+        assert(result==1) else $display("ERROR: Test Case 8: op=%b a=%h b=%h. Got result=%h, expected result=1.", op, a, b, result);
+        assert(zero==0) else $display("ERROR: Test Case 8: op=%b a=%h b=%h. Got zero=%h, expected zero=0", op, a, b, zero);
+        
+        a=5; b=1; op=SLT; #10;
+        assert(result==0) else $display("ERROR: Test Case 9: op=%b a=%h b=%h. Got result=%h, expected result=0.", op, a, b, result);
+        assert(zero==1) else $display("ERROR: Test Case 9: op=%b a=%h b=%h. Got zero=%h, expected zero=1", op, a, b, zero);
+        
+        //////////
+        // AND
+        //////////
+        a=9; b=14; op=AND; #10;
+        assert(result==8) else $display("ERROR: Test Case A: op=%b a=%h b=%h. Got result=%h, expected result=8.", op, a, b, result);
+        assert(zero==0) else $display("ERROR: Test Case 8: op=%b a=%h b=%h. Got zero=%h, expected zero=0", op, a, b, zero);
         
     end
 endmodule
