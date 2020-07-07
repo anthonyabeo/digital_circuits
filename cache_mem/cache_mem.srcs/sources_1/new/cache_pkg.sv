53ad1c0dbf9e7eb330606a48de4918ea2dc6fcb2
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-07-07 13:54:51 +0000

    upgrade package with required data structures

--- cache_mem/cache_mem.srcs/sources_1/new/cache_pkg.sv
+++ cache_mem/cache_mem.srcs/sources_1/new/cache_pkg.sv
@@ -21,23 +21,77 @@
 
 
 package cache_pkg;
-    typedef enum logic [6:0] {
-        LOAD = 7'b0000011,
-        STORE = 7'b0100011
-    } MemOpCode;
     
+    /////////////////////////////////////
+    // Structure for tag and data tables 
+    /////////////////////////////////////
+    parameter int TAG_MSB = 31;
+    parameter int TAG_LSB = 14;
     
+    // Entry into the cache tag table.
+    typedef struct packed {
+        logic                   valid;  // valid bit
+        logic                   dirty;  // dirty bit
+        logic [TAG_MSB:TAG_LSB] tag;    // Tag bits
+    } TagEntry;    
+    
+    // structre for cache memory request
+    typedef struct {
+        logic [9:0] index;      // 10-bit write indx
+        logic       we;         // write-enable
+    } CacheRequest;
+    
+    // 4-word (128-bit) block/line data.
+    typedef logic [127:0] Block;
+    
+    ////////////////////////////////////////////////////////
+    // Structures for the CPU <-> Cache Controler interface.
+    ////////////////////////////////////////////////////////
+    
+    // CPU Reqest (CPU->cache controller)
+    typedef struct {
+        logic [31:0] addr;      // 32-bit request addr
+        logic [31:0] data;      // 32-bit request data (used for STORE commands)
+        logic        rw;        // command type: 0 => read. 1 => write
+        logic        valid;     // request is valid       
+    } CPURequest;
+    
+    // Cache result(cache controller->cpu)
+    typedef struct {
+        logic [31:0] data;      // 32-bit data
+        logic        ready;     // result is ready
+    } CPUResult;
+     
+     
+     ///////////////////////////////////////////////////////
+     // Structres for cache controller <-> Memory interface
+     ///////////////////////////////////////////////////////
+     
+     // Memory request (cache controller -> memory)
+     typedef struct {
+        logic [31:0]   addr;    // request address
+        logic [127:0]  data;    // 128-bit data (used STORE commands)
+        logic          rw;      // command type: 0 => read. 1 => write
+        logic          valid;   // request is valid
+     } MemRequest;
+     
+     // Memory controller response (memory -> cache controller)
+     typedef struct {
+        Block data;     // 128-bit read back data
+        logic ready;    // data is ready
+     } MemResponse;
+     
+     ///////////////////////////////////////////
+     // FSM Controller specific structures
+     ///////////////////////////////////////////
+     
+    // One-Hot Encoding for states in FSM
     typedef enum logic [3:0] {
         IDLE        = 4'b0001,
         COMPARE_TAG = 4'b0010,
         ALLOCATE    = 4'b0100,
         WRITE_BACK  = 4'b1000
     } State;
+  
     
-    typedef struct packed {
-        logic valid, dirty;
-        logic [22:0] tag;
-        logic [31:0] data;
-    }  Block;
-    
-endpackage
+endpackage: cache_pkg
