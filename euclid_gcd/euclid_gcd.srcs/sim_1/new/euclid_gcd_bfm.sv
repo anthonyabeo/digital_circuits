ea5953fa8fe7920c8e6a972462740680f41c8580
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-07-10 18:42:12 +0000

    bus interface for testing

---
+++ euclid_gcd/euclid_gcd.srcs/sim_1/new/euclid_gcd_bfm.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/10/2020 06:07:42 PM
+// Design Name: 
+// Module Name: euclid_gcd_bfm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+interface EuclidGCD_BFM;
+    logic clk, rst;
+    logic [7:0] a, b, result;
+    
+    
+endinterface: EuclidGCD_BFM
