57b3207a26750761e4a1a0516212509649a3607f
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-07-10 18:41:30 +0000

    enhance test coverage fxnality

---
+++ euclid_gcd/euclid_gcd.srcs/sim_1/new/coverage.svh
@@ -0,0 +1,13 @@
+class Coverage;
+    
+    virtual EuclidGCD_BFM bfm;
+    
+    function new(virtual EuclidGCD_BFM b);
+        bfm = b;
+    endfunction: new
+    
+    task execute();
+    
+    endtask;
+    
+endclass: Coverage
