0cf09e19b4a18938604d37fc73fd922316af710a
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-07-10 18:40:09 +0000

    encapsulate test result checking

---
+++ euclid_gcd/euclid_gcd.srcs/sim_1/new/checker.svh
@@ -0,0 +1,14 @@
+
+class Checker;
+
+    virtual EuclidGCD_BFM bfm;
+    
+    function new(virtual EuclidGCD_BFM b);
+        bfm = b;
+    endfunction: new
+    
+    task execute();
+    
+    endtask;
+    
+endclass: Checker
