02cd7949a0d447163ffd23aa6f24dd1d43d91fda
A. Anabila Abeo <anthonyabeo@gmail.com>
2020-07-11 11:47:59 +0000

    generate input data for test

--- euclid_gcd/euclid_gcd.srcs/sim_1/new/stimulus.svh
+++ euclid_gcd/euclid_gcd.srcs/sim_1/new/stimulus.svh
@@ -7,7 +7,28 @@ class Stimulus;
         bfm = b;
     endfunction: new
     
-    task execute();
     
-    endtask;
+    protected function byte get_data();
+        bit [1:0] zero_ones;
+        zero_ones = $random;
+        if(zero_ones == 2'b00)
+            return 8'h00;
+        else if(zero_ones == 2'b11)
+            return 8'hFF;
+        else
+            return $random;
+            
+    endfunction: get_data
+    
+    
+    task execute();
+        byte ia, ib, result;
+        
+        ia = get_data();
+        ib = get_data();
+        
+        bfm.stimulate(ia, ib, result);
+        bfm.reset();
+    endtask: execute
+    
 endclass: Stimulus
